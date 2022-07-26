49a79ade4f39c65e4824e50d8f5e285b85beb9be
Pamela <abrepamm@gmail.com>
2022-07-26 13:24:55 -0300

    iniciando pagina de principal

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
+// HEADER
 const btnMobile = document.getElementById('btn-mobile');
+const linkNav = document.getElementsByTagName('a');
 
 function toggMenu (event) {
   if(event.type === 'touchstart') event.preventDefault()
   const nav = document.getElementById('header-nav');
   nav.classList.toggle('active');
-
 }
 
 btnMobile.addEventListener('click', toggMenu);
 btnMobile.addEventListener('touchstart', toggMenu);
+
+for (let index = 0; index < linkNav.length; index++) {
+  const link = linkNav[index];
+ link.addEventListener('click', toggMenu);
+ link.addEventListener('touchstart', toggMenu);
+}
+// __________________________________________________________________________________________________
